f4ff24ffc3af158571343e69dcc92637c8228f60
mohamed0_0Hesham <mohamed.hesham@nweave.com>
2023-05-31 17:50:40 +0300

    dependencies init

--- app/src/main/java/com/mohamed0_0hesham/weatherApp/MainActivity.kt
+++ app/src/main/java/com/mohamed0_0hesham/weatherApp/MainActivity.kt
@@ -2,11 +2,22 @@ package com.mohamed0_0hesham.weatherApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.Mohamed0_0hesham.weatherapp.R
+import com.mohamed0_0hesham.weatherapp.R
+import com.mohamed0_0hesham.weatherapp.databinding.ActivityMainBinding
+import meow.bottomnavigation.MeowBottomNavigation
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        bottomNavInit()
+    }
+    private fun bottomNavInit(){
+        val bottomNavigation = binding.bottomNav
+        bottomNavigation.add(MeowBottomNavigation.Model(1, R.drawable.ic_cloudy))
+        bottomNavigation.add(MeowBottomNavigation.Model(2, R.drawable.ic_drop))
+        bottomNavigation.add(MeowBottomNavigation.Model(3, R.drawable.ic_heavysnow))
     }
 }
