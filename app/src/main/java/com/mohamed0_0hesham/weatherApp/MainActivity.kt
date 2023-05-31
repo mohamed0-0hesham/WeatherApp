fbda16a8a2c926b6ff87efc067887532b60c8bc1
mohamed0_0Hesham <mohamed.hesham@nweave.com>
2023-05-31 15:12:19 +0300

    inti

---
+++ app/src/main/java/com/mohamed0_0hesham/weatherApp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mohamed0_0hesham.weatherApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.Mohamed0_0hesham.weatherapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
